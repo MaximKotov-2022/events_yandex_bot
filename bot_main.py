a492c1b36397726a89da1af0d4cb3e5a89a218dc
Максим Котов <KM4252010@yandex.ru>
2023-04-02 22:11:07 +0300

    Decompose process_information_parsing and send_message. Add buttons and event handling.

--- bot_main.py
+++ bot_main.py
@@ -1,9 +1,11 @@
 import os
+import time
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from telegram.ext import Filters, MessageHandler, Updater
+from telegram.ext import Filters, MessageHandler, Updater, CommandHandler
+from telegram import ReplyKeyboardMarkup
 
 load_dotenv()
 
@@ -24,7 +26,7 @@ def site_parsing():
 
 
 def processing_data_website():
-    """Обработка данных сайта после парсинга. Получение информации"""
+    """Обработка данных сайта после парсинга."""
     events = site_parsing()
     data_events = []
 
@@ -50,34 +52,85 @@ def processing_data_website():
     return data_events
 
 
-def send_message(update, context):
-    """Отправка сообщений.
-
-    Работает при отправке любого сообщения только один раз.
-    """
+def hi_say_first_message(update, context):
     chat = update.effective_chat
+    context.bot.send_message(
+        chat_id=chat.id,
+        text=(
+             "Привет! Это бот для получения информации о событиях Яндекса.\n"
+             "Тут можно узнать о всех мероприятиях, подписаться на "
+             "обновления, а также узнать о мероприятих в Москве."
+        )
+    )
+
+
+def process_information_parsing(update, context):
+    """Обработка информации после парсинга."""
     data = processing_data_website()
     text = []
-
     for event in data:
         date = event['date']
         name = event['name']
         site = event['site']
         text.append(f'Дата: {date}\nНазвание: {name}\nСайт: {site}\n\n\n')
+    return text
 
+
+def send_message(update, context):
+    """Отправка сообщений."""
+    text = process_information_parsing(update, context)
+    chat = update.effective_chat
+
+    buttons = ReplyKeyboardMarkup([
+        ['Подписаться на обновления'],
+        ['Все мероприятия', 'Меропрития в Москве'],
+    ],
+        resize_keyboard=True,
+    )
     context.bot.send_message(
         chat_id=chat.id,
         text=''.join(text),
+        reply_markup=buttons,
         disable_web_page_preview=True,
     )
 
-    text.clear()
+
+def subscribe_updates(update, context):
+    """Подписаться на все обновления мероприятий."""
+    old_data = send_message(update, context)
+    time.sleep(RETRY_PERIOD)
+    while True:
+        if old_data != send_message(update, context):
+            send_message(update, context)
+
+
+def events_city(update, context):
+    """Подписаться на обновления мероприятий в Москве."""
+    ...
 
 
 def main():
     """Запуск бота."""
     updater = Updater(token=TELEGRAM_TOKEN)
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, send_message))
+    updater.dispatcher.add_handler(CommandHandler(
+        'start', hi_say_first_message)
+    )
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.text('Все мероприятия'),
+        send_message
+        )
+    )
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.text('Подписаться на обновления'),
+        subscribe_updates
+        )
+    )
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.text('Меропрития в Москве'),
+        events_city
+        )
+    )
+
     updater.start_polling()
     updater.idle()
 
