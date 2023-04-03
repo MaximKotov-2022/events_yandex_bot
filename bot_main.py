4be35487659969d3f1cc413023ad4d92049120ea
Максим Котов <KM4252010@yandex.ru>
2023-04-03 18:49:54 +0300

    Correct errors. Make under OOP

--- bot_main.py
+++ bot_main.py
@@ -14,120 +14,119 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 3600
 
 
-def site_parsing():
-    """Получение данных сайта (парсинг)."""
-    url = "https://events.yandex.ru/"
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    events = soup.find(class_='events__container')
+class GetData:
+    def site_parsing():
+        """Получение данных сайта (парсинг)."""
+        url = "https://events.yandex.ru/"
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        events = soup.find(class_='events__container')
 
-    return events
+        return events
 
+    def processing_data_website():
+        """Обработка данных сайта после парсинга."""
+        events = GetData.site_parsing()
+        data_events = []
 
-def processing_data_website():
-    """Обработка данных сайта после парсинга."""
-    events = site_parsing()
-    data_events = []
+        for event in events:
+            if len(event) != 0:
+                date = event.find(class_='event-card__date').text
+                name = event.find(class_='event-card__title').text
+                if 'https' not in event.find('a').get('href'):
+                    site = (
+                        'https://events.yandex.ru'
+                        + event.find('a').get('href')
+                    )
+                else:
+                    site = event.find('a').get('href')
 
-    for event in events:
-        if len(event) != 0:
-            date = event.find(class_='event-card__date').text
-            name = event.find(class_='event-card__title').text
-            if 'https' not in event.find('a').get('href'):
-                site = (
-                    'https://events.yandex.ru' + event.find('a').get('href')
+                data_events.append(
+                    {
+                        "date": date,
+                        "name": name,
+                        "site": site,
+                    }
                 )
-            else:
-                site = event.find('a').get('href')
 
-            data_events.append(
-                {
-                    "date": date,
-                    "name": name,
-                    "site": site,
-                }
+        return data_events
+
+
+class ProcessingDataBot():
+    def hi_say_first_message(update, context):
+        chat = update.effective_chat
+        context.bot.send_message(
+            chat_id=chat.id,
+            text=(
+                "Привет! Это бот для получения информации о событиях"
+                "Яндекса.\n"
+                "Тут можно узнать о всех мероприятиях, подписаться на "
+                "обновления, а также узнать о мероприятих в Москве."
             )
-
-    return data_events
-
-
-def hi_say_first_message(update, context):
-    chat = update.effective_chat
-    context.bot.send_message(
-        chat_id=chat.id,
-        text=(
-             "Привет! Это бот для получения информации о событиях Яндекса.\n"
-             "Тут можно узнать о всех мероприятиях, подписаться на "
-             "обновления, а также узнать о мероприятих в Москве."
         )
-    )
 
+    def process_information_parsing(update, context):
+        """Обработка информации после парсинга."""
+        data = GetData.processing_data_website()
+        text = []
+        for event in data:
+            date = event['date']
+            name = event['name']
+            site = event['site']
+            text.append(f'Дата: {date}\nНазвание: {name}\nСайт: {site}\n\n\n')
+        return text
 
-def process_information_parsing(update, context):
-    """Обработка информации после парсинга."""
-    data = processing_data_website()
-    text = []
-    for event in data:
-        date = event['date']
-        name = event['name']
-        site = event['site']
-        text.append(f'Дата: {date}\nНазвание: {name}\nСайт: {site}\n\n\n')
-    return text
+    def send_message(update, context):
+        """Отправка сообщений."""
+        text = ProcessingDataBot.process_information_parsing(update, context)
+        chat = update.effective_chat
 
+        buttons = ReplyKeyboardMarkup([
+            ['Подписаться', 'Отписаться'],
+            ['Все мероприятия', 'Мероприятия в Москве'],
+        ],
+            resize_keyboard=True,
+        )
+        context.bot.send_message(
+            chat_id=chat.id,
+            text=''.join(text),
+            reply_markup=buttons,
+            disable_web_page_preview=True,
+        )
 
-def send_message(update, context):
-    """Отправка сообщений."""
-    text = process_information_parsing(update, context)
-    chat = update.effective_chat
+    def subscribe_updates(update, context):
+        """Подписаться на все обновления мероприятий."""
+        old_data = ProcessingDataBot.send_message(update, context)
+        time.sleep(RETRY_PERIOD)
+        while True:
+            if old_data != GetData.send_message(update, context):
+                GetData.send_message(update, context)
 
-    buttons = ReplyKeyboardMarkup([
-        ['Подписаться на обновления'],
-        ['Все мероприятия', 'Меропрития в Москве'],
-    ],
-        resize_keyboard=True,
-    )
-    context.bot.send_message(
-        chat_id=chat.id,
-        text=''.join(text),
-        reply_markup=buttons,
-        disable_web_page_preview=True,
-    )
-
-
-def subscribe_updates(update, context):
-    """Подписаться на все обновления мероприятий."""
-    old_data = send_message(update, context)
-    time.sleep(RETRY_PERIOD)
-    while True:
-        if old_data != send_message(update, context):
-            send_message(update, context)
-
-
-def events_city(update, context):
-    """Подписаться на обновления мероприятий в Москве."""
-    ...
+    def events_city(update, context):
+        """Подписаться на обновления мероприятий в Москве."""
+        ...
 
 
 def main():
     """Запуск бота."""
     updater = Updater(token=TELEGRAM_TOKEN)
     updater.dispatcher.add_handler(CommandHandler(
-        'start', hi_say_first_message)
+        'start', ProcessingDataBot.hi_say_first_message)
     )
     updater.dispatcher.add_handler(MessageHandler(
         Filters.text('Все мероприятия'),
-        send_message
+        ProcessingDataBot.send_message
         )
     )
     updater.dispatcher.add_handler(MessageHandler(
-        Filters.text('Подписаться на обновления'),
-        subscribe_updates
+        Filters.text('Подписаться'),
+        ProcessingDataBot.subscribe_updates
         )
     )
     updater.dispatcher.add_handler(MessageHandler(
-        Filters.text('Меропрития в Москве'),
-        events_city
+        Filters.text('Мероприятия в Москве'),
+        ProcessingDataBot.events_city
         )
     )
 
