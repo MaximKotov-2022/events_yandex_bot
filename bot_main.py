ac3fe1314c29cc7fb243038a67b7bee6bc690396
Максим Котов <KM4252010@yandex.ru>
2023-04-07 20:17:36 +0300

    Apply isort import filtering.

--- bot_main.py
+++ bot_main.py
@@ -1,11 +1,11 @@
 import os
-import time
 import threading
+import time
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from telegram import ReplyKeyboardMarkup, ParseMode
+from telegram import ParseMode, ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 load_dotenv()
