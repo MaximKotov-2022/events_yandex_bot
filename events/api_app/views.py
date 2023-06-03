3c10828f1e817adf6b8679dff9b29ed819b475b5
Максим Котов <KM4252010@yandex.ru>
2023-06-03 12:03:13 +0300

    Add serialization. Configure the documentation.

--- events/api_app/views.py
+++ events/api_app/views.py
@@ -1,14 +1,16 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import EventsSerializer
 from events.bot_main import GetData
+from rest_framework import status
 
 
 @api_view(['GET'])
 def api_events(request):
     if request.method == 'GET':
         data = GetData.processing_data_website()
-        serializer = EventsSerializer(data=data, many=True)
-        serializer.is_valid()
 
-        return JsonResponse(serializer.data, safe=False)
+        serializer = EventsSerializer(data=data, many=True)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- events/events/urls.py
+++ events/events/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title="Example API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api_app.urls')),
+    path('schema', schema_view),
 ]
